81ddbfa02cd0b6074b210b8cfb9dbb8aa81b6b21
manaken01 <82286286+manaken01@users.noreply.github.com>
2023-03-15 11:11:40 -0600

    a

--- Assets/SpawnerScript.cs
+++ Assets/SpawnerScript.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class SpawnerScript : MonoBehaviour
 {
     public GameObject objeto;
+    public GameObject pistola;
 
     public void InstantiateNewObject() {
         //GameObject taza = new GameObject("taza");
         Instantiate(objeto, new Vector3(0.746f, 0.883f, 1.222f), Quaternion.identity);
-
+        gun bala = pistola.GetComponent<gun>();
+        bala.bullet = objeto;
     }
 
 
