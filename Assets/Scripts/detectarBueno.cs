ab49efbacd9b36c515bb9849d7a623d380c9ea54
manaken01 <82286286+manaken01@users.noreply.github.com>
2023-05-25 12:46:56 -0600

    musica en mi escena

--- Assets/Scripts/detectarBueno.cs
+++ Assets/Scripts/detectarBueno.cs
@@ -8,6 +8,7 @@ public class detectarBueno : MonoBehaviour
     public bool obj1;
     public bool obj2;
     public bool obj3;
+    public AudioSource audioSource;
 
     // Update is called once per frame
     void Update()
@@ -20,6 +21,7 @@ public class detectarBueno : MonoBehaviour
 
     IEnumerator cargarScena() {
         yield return new WaitForSeconds(4);
+        audioSource.mute = true;
         SceneManager.LoadScene("Escena 3", LoadSceneMode.Single);
     }
 }
