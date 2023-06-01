7ac26567563828ce16b42c53ad39566e9a4d9f9f
manaken01 <82286286+manaken01@users.noreply.github.com>
2023-06-01 11:44:28 -0600

    a

--- Assets/Escena Diego/Scripts/RayCastController.cs
+++ Assets/Escena Diego/Scripts/RayCastController.cs
@@ -4,30 +4,26 @@ using UnityEngine;
 
 public class RayCastController : MonoBehaviour
 {
-    public GameObject currentHitObject;
+  public GameObject currentHitObject;
+    
 
     // Update is called once per frame
     void FixedUpdate()
     {
         float _rotatespeed = 100f;
         RaycastHit hit;
+        var leftHandDevices = new List<UnityEngine.XR.InputDevice>();
+        UnityEngine.XR.InputDevice device = leftHandDevices[0];
 
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
         {
           var selectedObject = hit.transform;
           if (selectedObject != null)
           {
-            if (Input.GetKey(KeyCode.X))
+            bool triggerValue;
+            if (device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.primaryButton, out triggerValue) && triggerValue)
             {
               gameObject.transform.Rotate(new Vector3(0f, 0f, _rotatespeed) * Time.deltaTime, Space.World);
-            }
-            if (Input.GetKey(KeyCode.Y))
-            {
-              gameObject.transform.Rotate(new Vector3(0f, _rotatespeed, 0f) * Time.deltaTime, Space.World);
-            }
-            if (Input.GetKey(KeyCode.Z))
-            {
-              gameObject.transform.Rotate(new Vector3(_rotatespeed, 0f, 0f) * Time.deltaTime, Space.World);
             }
           }
         }
